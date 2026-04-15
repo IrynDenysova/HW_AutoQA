83433c829277e5c739043da87c9163243cf039c7
IrynDenysova <iriska.ermak@gmail.com>
2026-04-15 19:37:50 +0200

    add HW1

---
+++ HW_1/test_simple_math.py
@@ -0,0 +1,12 @@
+from simple_math import SimpleMath
+import pytest
+
+@pytest.fixture
+def simple_math():
+    return SimpleMath()
+
+def test_square(simple_math):
+    assert simple_math.square(2) == 4
+
+def test_cube(simple_math):
+    assert simple_math.cube(-3) == -27
